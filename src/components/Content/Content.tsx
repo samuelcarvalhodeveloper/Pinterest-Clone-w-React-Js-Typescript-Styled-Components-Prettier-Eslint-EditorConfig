4c5273c2c3e32b03873f75f86db0d643b93fe7c6
Samuel Carvalho <personal.samuelcarvalho@gmail.com>
2022-11-09 07:27:07 -0300

    More cards

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -89,6 +89,48 @@ function Content() {
           creator="Creator Name"
           link="https://source.unsplash.com/random/236x300"
         />
+        <Card
+          image="https://source.unsplash.com/random/236x300"
+          title="Lorem Ipsum dolor"
+          creator="Creator Name"
+          link="https://source.unsplash.com/random/236x300"
+        />
+        <Card
+          image="https://source.unsplash.com/random/236x300"
+          title="Lorem Ipsum dolor"
+          creator="Creator Name"
+          link="https://source.unsplash.com/random/236x300"
+        />
+        <Card
+          image="https://source.unsplash.com/random/236x300"
+          title="Lorem Ipsum dolor"
+          creator="Creator Name"
+          link="https://source.unsplash.com/random/236x300"
+        />
+        <Card
+          image="https://source.unsplash.com/random/236x300"
+          title="Lorem Ipsum dolor"
+          creator="Creator Name"
+          link="https://source.unsplash.com/random/236x300"
+        />
+        <Card
+          image="https://source.unsplash.com/random/236x300"
+          title="Lorem Ipsum dolor"
+          creator="Creator Name"
+          link="https://source.unsplash.com/random/236x300"
+        />
+        <Card
+          image="https://source.unsplash.com/random/236x300"
+          title="Lorem Ipsum dolor"
+          creator="Creator Name"
+          link="https://source.unsplash.com/random/236x300"
+        />
+        <Card
+          image="https://source.unsplash.com/random/236x300"
+          title="Lorem Ipsum dolor"
+          creator="Creator Name"
+          link="https://source.unsplash.com/random/236x300"
+        />
       </div>
     </ContentStyled>
   );
